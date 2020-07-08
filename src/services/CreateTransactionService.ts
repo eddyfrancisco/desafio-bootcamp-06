acae5d13200a083c92bc4427cb2d15f88814c422
Eddy Francisco <esilva.prestador@smartbpo.io>
2020-07-07 21:48:47 -0300

    Desafio 06 completo

---
+++ src/services/CreateTransactionService.ts
@@ -0,0 +1,57 @@
+import AppError from '../errors/AppError';
+
+import { getCustomRepository, getRepository } from 'typeorm';
+
+import Transaction from '../models/Transaction';
+
+import TransactionRepository from '../repositories/TransactionsRepository';
+import Caterogy from '../models/Category';
+
+
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+
+}
+
+class CreateTransactionService {
+  public async execute({ title, value, type, category }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+    const categoryRepository = getRepository(Caterogy);
+
+    const { total } = await transactionRepository.getBalance();
+
+    if(type === 'outcome' && total < value) {
+      throw new AppError("You cannot have enought balance");
+    }
+
+    let transactionCategory = await categoryRepository.findOne({
+      where: {
+        title: category
+      }
+    });
+
+    if (!transactionCategory) {
+      transactionCategory = categoryRepository.create({
+        title: category
+      })
+
+      await categoryRepository.save(transactionCategory);
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category: transactionCategory
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
